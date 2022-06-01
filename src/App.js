477eb9ba1f43323278724dca7a99a8f402a38878
AlexMalla <mallamacialex@gmail.com>
2022-06-01 18:01:02 +0200

    ex JSX 02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+// Exercise 01
 
 let hello = <h1>Hello, World!</h1>;
+
+//Exercise 02
+function Name(param) {
+  return <h1> Hello, {param}</h1>;
+}
+
+function App() {
+  return <div className="App">{Name("Sara")}</div>;
+}
 
 export default App;
