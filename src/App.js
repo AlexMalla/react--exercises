a6a7192e9ad6637297a354976774fe9c8bbbebe8
AlexMalla <mallamacialex@gmail.com>
2022-06-01 18:11:22 +0200

    ex JSX 05

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import "./App.css";
 let hello = <h1>Hello, World!</h1>;
 
 //Exercise 02
+/*
 function Name(param) {
   return <h1> Hello, {param}</h1>;
 }
 
 function App() {
   return <div className="App">{Name("Sara")}</div>;
+}
+*/
+//Exercise 05
+
+function Sum(a, b) {
+  return <h2> {a + b} </h2>;
+}
+
+function App() {
+  return <h2 className="App"> {Sum(9, 8)} </h2>;
 }
 
 export default App;
