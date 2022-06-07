45ad9fe1e4c6f6aa3f46d1d1dec7668d74017464
AlexMalla <mallamacialex@gmail.com>
2022-06-07 13:16:50 +0200

    ex Props 04

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Welcome from "./Welcome";
 
+const StrongLuigi = <strong>Luigi</strong>;
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <Welcome name="Alex" age="22" />
+        <Welcome name={StrongLuigi} age="18" />
       </div>
     );
   }
