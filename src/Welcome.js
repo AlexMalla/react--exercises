71e1d998331dba85337cf71886a677409934a5b6
AlexMalla <mallamacialex@gmail.com>
2022-06-13 17:21:33 +0200

    Ex Conditional Rendering 03

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,7 +6,10 @@ class Welcome extends React.Component {
     return (
       <div>
         <p> Welcome {this.props.name}</p>
-        <p> {this.props.age && <Age age={this.props.age} />}</p>
+
+        {this.props.age > 18 && this.props.age < 65 && (
+          <Age age={this.props.age} />
+        )}
       </div>
     );
   }
