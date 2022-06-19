e16e16f3f2c72878c1fdf7f1955db4a3b2011416
AlexMalla <mallamacialex@gmail.com>
2022-06-19 17:37:36 +0200

    Ex Forms 02

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import InteractiveWelcome from "./InteractiveWelcome";
+import Login from "./Login";
 
 function App() {
   return <div className="App">
-    <InteractiveWelcome />
+    <Login/>
   </div>;
 }
 
