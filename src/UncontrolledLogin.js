146263272259625e2fa3d2cb10d10cb85851fa29
AlexMalla <mallamacialex@gmail.com>
2022-06-19 17:45:16 +0200

    Ex Forms 05

---
+++ src/UncontrolledLogin.js
@@ -0,0 +1,35 @@
+import React, {createRef} from "react";
+
+class UncontrolledLogin extends React.Component {
+
+    _formRef = createRef()
+
+handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const username = event.target.elements.username.value
+    const password = event.target.elements.password.value
+    const remember = event.target.elements.remember.value
+
+    console.log({
+        username,
+        password,
+        remember
+    })
+}
+
+render() {
+    return (
+        <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
+            <input name="username" placeholder="Username" />
+            <input name="password" type="password" placeholder="Password" />
+            <input name="remember" type="checkbox" />
+            <span>Remember me</span>
+            <button type="submit">Login</button>
+            <button type="reset">Reset</button>
+        </form>
+    )
+}
+
+}
+export default UncontrolledLogin
