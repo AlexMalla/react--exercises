6324233ed34711bb51d7d95b7c51c656eb54825a
AlexMalla <mallamacialex@gmail.com>
2022-06-19 17:42:25 +0200

    Ex Forms 03

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,10 @@
 import React from "react";
-
-
 class Login extends React.Component {
-
     state = {
         username: '',
         password: '',
-        checkbox : false
+        checkbox : false,
+        login: true
     }
 
     HandleSubmit = (event) => {
@@ -14,7 +12,6 @@ class Login extends React.Component {
         const value = event.target.value
         const checkbox= event.target.checked
         const type = event.target.type
-
         this.setState({
             [name]: type === 'checkbox'
              ? checkbox
@@ -22,17 +19,28 @@ class Login extends React.Component {
         })
     }
 
+        onLogin = (event) => {
+            console.log(this.state)
+
+        }
 
     render() {
-        return <div>
+        return <form>
             <input type="text" name="username" value={this.state.username} onChange={this.HandleSubmit} placeholder="Username" required />
             <input type="password" name="password" value={this.state.password} onChange={this.HandleSubmit} placeholder="Password" required />
             <input type="checkbox" name="checkbox" value={this.state.checkbox} onChange={this.HandleSubmit} />
             <span>remember me</span>
-             </div>
+           
+            <button type="submit"
+                    onClick={this.onLogin}
+                    disabled={(this.state.password === "" || this.state.username === ""
+                    ? true
+                    : false
+                    )}>
+                        Login
+                        </button>
+             </form>
     }
 }
 
-
-
-export default Login 
+export default Login
