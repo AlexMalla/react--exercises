c70625ce6766bbad4f3850e84e35def2b65a372d
AlexMalla <mallamacialex@gmail.com>
2022-06-26 18:11:52 +0200

    Ex Function Components 03

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Sum from "./Sum";
 
 function App() {
   return <div className="App">
-    <Sum numbers={[1, 2, 3, 4]} />
+    <Sum />
   </div>;
 }
 
--- src/Sum.js
+++ src/Sum.js
@@ -1,8 +1,8 @@
-export function Sum(props) {
+export function Sum({ numbers = [1, 2, 3, 4] }) {
 
     return (
         <div>
-            <h1>{props.numbers.reduce((a, b) => a + b)}</h1>
+            <h1>{numbers.reduce((a, b) => a + b)}</h1>
         </div>
     )
 }
