8c6cdeb4eed1c935d4397ca55d694fc2e9f32ce3
AlexMalla <mallamacialex@gmail.com>
2022-06-26 17:10:49 +0200

    Ex Context 01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import DisplayLanguage from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext"
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  state = {
+    language: 'en'
+  }
+
+  SelectLanguage = (event) => {
+    this.setState((state) => {
+      return { language: state.language = event.target.value }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <select value={this.state.language} onChange={this.SelectLanguage}>
+          <option value='en'>English</option>
+          <option value='it'>Italiano</option>
+        </select>
+        <LanguageContext.Provider value={this.state.language}>
+          <DisplayLanguage>
+          </DisplayLanguage>
+        </LanguageContext.Provider>
+      </div >
+    );
+  }
 }
 
-export default App;
+export default App
