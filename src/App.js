95019a23436c20634c60af803828506ab1106415
AlexMalla <mallamacialex@gmail.com>
2022-06-27 10:04:50 +0200

    Ex useState 01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import ClickCounter from "./ClickCounter"
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <ClickCounter />
+  </div>;
 }
 
 export default App;
