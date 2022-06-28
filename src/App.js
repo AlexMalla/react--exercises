0e38dd94ec4f193e9e17aa6405a0ae1f089c3c7c
AlexMalla <mallamacialex@gmail.com>
2022-06-28 18:22:35 +0200

    Ex useEffect 03

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import "./App.css";
-import Counter from "./Counter"
-import { useState } from "react"
+import GithubUser from "./GithubUser";
+
 
 function App() {
-
-  const [toggle, setToggle] = useState(true)
-
-
-
-  function onCounterChange(count) {
-    console.log(`The counter is ${count}`)
-  }
-
-  function handleButton() {
-    setToggle(s => !s)
-  }
-
-  return <div className="App">
-    {toggle && <Counter onCounterChange={onCounterChange} />}
-    <button onClick={handleButton} >Unmount</button>
-
-  </div>;
+  return (
+    <div>
+      <GithubUser username={'AlexMalla'} />
+    </div>
+  )
 }
 
 export default App;
