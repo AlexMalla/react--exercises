5f54b7aca59202ed1691fbc5d5063738e4458c2c
AlexMalla <mallamacialex@gmail.com>
2022-06-30 19:08:50 +0200

    Ex React Router 02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Welcome from "./Welcome";
 import { Routes, Route } from "react-router-dom"
+import Counter from "./Counter";
 
 function App() {
   return <div className="App">
     <Routes>
       <Route path="/" element={<Welcome name={'Alex'} />} />
+      <Route path="/counter" element={<Counter />} />
     </Routes>
   </div>;
 }
