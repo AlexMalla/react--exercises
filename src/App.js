8e24a0925e5516b7ebc117bcf101acbd9973f6ec
AlexMalla <mallamacialex@gmail.com>
2022-06-30 21:25:21 +0200

    Ex ReactRouter 04

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Welcome from "./Welcome";
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom"
 import Counter from "./Counter";
 import ShowGithubUser from "./ShowGithubUser"
 
+const style = {
+  margin: "10px",
+}
+
 function App() {
   return <div className="App">
+    <div>
+      <Link style={style} to="/" > Welcome</Link>
+      <Link style={style} to="/counter" > Counter</Link>
+      <Link style={style} to="/githubuser/AlexMalla" >Github user</Link>
+    </div>
     <Routes>
       <Route path="/" element={<Welcome name={'Alex'} />} />
       <Route path="/counter" element={<Counter />} />
--- src/GithubUser.js
+++ src/GithubUser.js
@@ -25,7 +25,7 @@ function GithubUser({ username = "AlexMalla" }) {
     return (
         <div>
             {error && <h1>Error</h1>}
-            {data && <img src={data.avatar_url} alt="avatar"></img>}
+            {data && <img src={data.avatar_url} alt="avatar" width={100}></img>}
             {data && <h1>Hi, my name is {data.name}.</h1>}
             {data && <h2>I live in {data.location}</h2>}
             {data && <h3>My GitHub is {data.html_url}</h3>}
