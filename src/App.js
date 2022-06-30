941c5ccb33109754861d8447b92845db53bccbc4
AlexMalla <mallamacialex@gmail.com>
2022-06-30 16:49:49 +0200

    Ex React Router 01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Welcome from "./Welcome";
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Routes>
+      <Route path="/" element={<Welcome name={'Alex'} />} />
+    </Routes>
+  </div>;
 }
 
 export default App;
